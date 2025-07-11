7f922715473a1bb8ad6eacc74f9dc13bf8a7639e
Gleb <filingleb06@gmail.com>
2025-07-11 16:47:18 +0200

    work vers

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const sendBtn = document.querySelector(".translate-box__active-Btn")
+const resultInput = document.querySelector(".input_en")
+const enterInput = document.querySelector(".input_ru")
+
+
+async function translate(sentWord) {
+
+    const fromLang = "ru"
+    const toLang = "en"
+    const encodedText = encodeURIComponent(sentWord)
+    const url = `https://api.mymemory.translated.net/get?q=${encodedText}&langpair=${fromLang}|${toLang}`;
+
+    try{
+
+        const response = await fetch(`${url}`)
+
+        if(!response.ok){
+            throw new Error(`Server error: ${response.status}`)
+        } 
+
+        const data = await response.json()
+        console.log(data)
+
+        const translatedText = data.responseData.translatedText
+        if(data.responseStatus == "403"){
+            enterInput.focus()
+        }else{
+            resultInput.innerHTML = translatedText 
+        }
+
+    }catch(error){
+        console.error(error)
+    }
+
+}
+
+
+sendBtn.addEventListener("click" , () => {
+    const sentWord = document.querySelector(".input_ru").value
+    translate(sentWord)
+})
